f6285bc3fce16481a4ac18b87ecb22554bf308b3
liaHi19 <gn.natalia19@gmail.com>
2022-10-17 19:57:27 +0300

    [#0]Add start files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next"
+import React from 'react'
+
+const HomePage:NextPage = () => {
+  return (
+    <div>
+      Natalia
+    </div>
+  )
+}
+
+export default HomePage
+
+
