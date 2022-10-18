f5e44f786804e21f03b6527956ed9d6c736e50e9
liaHi19 <gn.natalia19@gmail.com>
2022-10-18 08:08:26 +0300

    [#1] Add Layout, SearchInput

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import SearchInput from "./SearchInput";
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="logo reddit" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="logo reddit"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      <SearchInput />
+      {/* Directory */}
+      {/* RightContent */}
+    </Flex>
+  );
+};
+export default Navbar;
